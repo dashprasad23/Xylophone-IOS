8649c914a0a7a9e7e318f8fb068229d14c8740bb
Debiprasad.Dash <Debiprasad.Dash@navihealth.com>
2023-05-24 19:07:23 +0530

    Init

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,17 +5,28 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5;
+       playSound(name: sender.currentTitle!, playBtn: sender)
+    }
+    
+    func playSound(name: String, playBtn: UIButton) {
+        let url = Bundle.main.url(forResource: name, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+        playBtn.alpha = 1
+    }
+    
 }
 
